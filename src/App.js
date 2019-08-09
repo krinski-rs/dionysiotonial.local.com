d2ec20ce6bbbb9ae2527504bf95b86d48c42447f
Reinaldo Freitas <reinaldo.freitas@vogeltelecom.com>
2019-08-09 11:04:05 -0300

    início da atualização do template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import {
+	Router,
+	Switch,
+	Route,
+} from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Login from "./screens/login/Login";
+var hist = createBrowserHistory();
+class App extends Component {
+	render() {
+		return (
+			<Router history={hist}>
+				<Switch>
+			      <Route path="/" component={Login} />
+			    </Switch>
+			</Router>
+		);
+	}
 }
 
 export default App;
